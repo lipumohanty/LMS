fc3b6734c6239be28bacebc7f5697d39c8a5f3b9
lipumohanty <lipu123mohanty@gmail.com>
2017-01-23 10:51:39 -0800

    add_employee.php- calender added

--- GlobalDataUploder/employee/add_employee.php
+++ GlobalDataUploder/employee/add_employee.php
@@ -7,12 +7,18 @@ if (isset($_POST["submit"])) {
     MysqlConnection::insert(" tbl_employee", $_POST);
 }
 ?>
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+	  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+ <script>
+	  $( function() {
+	    $( "#dob" ).datepicker({ dateFormat: 'yy-mm-dd' });
+	  } );
+	   $( function() {
+	    $( "#join_date" ).datepicker({ dateFormat: 'yy-mm-dd' });
+	  } );
 
-<script>
-  $( function() {
-    $( "#join_date" ).datepicker();
-  } );
-  </script>
+	  </script>
 
 <div class="container-fluid">
     <div class="row-fluid">
@@ -75,7 +81,7 @@ if (isset($_POST["submit"])) {
                                 <div class="span11"  >
                                     <label class="control-label ">D.O.B&nbsp&nbsp;:&nbsp;</label>
                                     <div class="controls">
-                                        <input type="date" name="dob" value="" maxlength="255" class="span12"  required="" placeholder="" />
+                                        <input type="date" name="dob" id="dob" value="" maxlength="255" class="span12"  required="" placeholder="" />
                                     </div>
                                 </div>
                             </div>
