a8978d841a5822639afb05e1134ded0466d55067
lipumohanty <lipu123mohanty@gmail.com>
2017-01-25 02:16:14 -0800

    name update navigation.php

--- GlobalDataUploder/navigation.php
+++ GlobalDataUploder/navigation.php
@@ -1,9 +1,9 @@
 <a href="index.php" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
 <ul>
     <li class=""><a href="index.php?requestPage="><i class="icon icon-home"></i> <span>DASHBOARD</span></a> </li>
-    <li class=""> <a href="index.php?requestPage=add_employee"><i class="icon icon-magic"></i> <span>EMPLOYEE REGISTRATION</span></a></li>
-    <li class=""> <a href="index.php?requestPage=addleave_leave"><i class="icon icon-magic"></i> <span>ADD LEAVE</span></a></li>
-    <li class=""> <a href="index.php?requestPage=viewleave_leave"><i class="icon icon-film"></i> <span>VIEW LEAVE </span></a></li>
+    <li class=""> <a href="index.php?requestPage=add_employee"><i class="icon icon-magic"></i> <span>EMPLOYEE MANAGER</span></a></li>
+    <li class=""> <a href="index.php?requestPage=addleave_leave"><i class="icon icon-magic"></i> <span>LEAVE MANAGER</span></a></li>
+    <li class=""> <a href="index.php?requestPage=addleave_setting"><i class="icon icon-film"></i> <span>LEAVE SETTING MANAGER </span></a></li>
     <li class=""> <a href="index.php?requestPage=view_employee"><i class="icon icon-magic"></i> <span>LEAVE REPORT</span></a></li>
     <li class="submenu"  class=""> <a href=""><i class="icon icon-search"></i> <span>LOG-OUT</span></a></li>
 </ul>
