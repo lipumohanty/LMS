d443fcd142a6f9c9a4684d112b9cb288710b2669
lipumohanty <lipu123mohanty@gmail.com>
2017-01-29 07:09:30 -0800

    relevent changes

--- GlobalDataUploder/navigation.php
+++ GlobalDataUploder/navigation.php
@@ -1,10 +1,10 @@
 <a href="index.php" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
 <ul>
     <li class=""><a href="index.php?requestPage="><i class="icon icon-home"></i> <span>DASHBOARD</span></a> </li>
-    <li class=""> <a href="index.php?requestPage=view_employee"><i class="icon icon-magic"></i> <span>EMPLOYEE MANAGER</span></a></li>
-    <li class=""> <a href="index.php?requestPage=viewleave_leave"><i class="icon icon-magic"></i> <span>LEAVE MANAGER</span></a></li>
-    <li class=""> <a href="index.php?requestPage=viewaddleave_setting"><i class="icon icon-film"></i> <span>LEAVE SETTING </span></a></li>
-    <li class=""> <a href="index.php?requestPage=viewpredefined_setting"><i class="icon icon-magic"></i> <span>PREDEFINED LEAVE</span></a></li>
-     <li class=""> <a href="index.php?requestPage=add_leaveapproval"><i class="icon icon-magic"></i> <span> LEAVE REQUEST MANAGER</span></a></li>
+    <li class=""> <a href="index.php?requestPage=view_employee"><i class="icon icon-sign-blank"></i> <span>EMPLOYEE MANAGER</span></a></li>
+    <li class=""> <a href="index.php?requestPage=viewleave_leave"><i class="icon icon-sign-blank"></i> <span>LEAVE MANAGER</span></a></li>
+    <li class=""> <a href="index.php?requestPage=viewaddleave_setting"><i class="icon icon-sign-blank"></i> <span>LEAVE SETTING </span></a></li>
+    <li class=""> <a href="index.php?requestPage=viewpredefined_setting"><i class="icon icon-sign-blank"></i> <span>PREDEFINED LEAVE</span></a></li>
+     <li class=""> <a href="index.php?requestPage=add_leaveapproval"><i class="icon icon-sign-blank"></i> <span> LEAVE REQUEST MANAGER</span></a></li>
     <li class="submenu"  class=""> <a href=""><i class="icon icon-search"></i> <span>LOG-OUT</span></a></li>
 </ul>
