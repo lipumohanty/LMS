7df8f2fa54888f6474b8b01072ec670cebed3fa9
lipumohanty <lipu123mohanty@gmail.com>
2017-01-30 01:13:25 -0800

    @pallavi ui changes

--- GlobalDataUploder/setting/addleave_setting.php
+++ GlobalDataUploder/setting/addleave_setting.php
@@ -65,7 +65,7 @@ if (isset($_POST["submit"])) {
                                     <a href=""><button type="submit" name="submit" class="btn btn-success">SUBMIT</button>
                                         <button type="reset" class="btn btn-primary">RESET</button>
                                         <a href="index.php?requestPage=viewaddleave_setting"><button type="button" class="btn btn-info">VIEW</button></a>
-                                        <button type="submit" class="btn btn-danger">CANCEL</button>
+                                        <a href="index.php?requestPage=viewaddleave_setting"><button type="button" class="btn btn-danger">CANCEL</button></a>
                                 </div>
                             </center>
                         </div>
